20a1cd345728f8a52a5b251238601aa4419223ae
masterzht <zht1246@outlook.com>
2018-03-30 22:51:20 +0800

    update xxx

--- source/_posts/My-New-Post.md
+++
@@ -1,5 +0,0 @@
----
-title: My New Post
-date: 2018-03-30 17:55:53
-tags:
----
---
+++ source/_posts/jupyter_notebook.bat
@@ -0,0 +1,4 @@
+rem -- start_ipython_notebook_here.bat ---
+dir
+ipython notebook J:\web\temp\hexo\source\_posts
+pause
